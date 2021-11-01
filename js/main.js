5667d975dbdb84ec9ef2f1416bf55b0a0eb091a9
Lucas <lucasgola@outlook.com>
2021-10-31 22:09:53 -0300

    courses form

--- js/main.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 60) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-  }
-}
